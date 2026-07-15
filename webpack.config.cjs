be47ebfc0b65ff92f6239dae7860070208f7cd94
madhesh0727 <madhesh0727@gmail.com>
2026-07-15 16:16:19 +0530

    Fix Babel production runtime environment

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,60 +2,67 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/main.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new webpack.DefinePlugin({
-      'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || '')
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './src/main.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
     },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-    proxy: [
-      {
-        context: ['/api', '/static'],
-        target: 'http://127.0.0.1:5000',
-      },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              envName: isProduction ? 'production' : 'development'
+            }
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new webpack.DefinePlugin({
+        'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || '')
+      })
     ],
-  },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+      hot: true,
+      proxy: [
+        {
+          context: ['/api', '/static'],
+          target: 'http://127.0.0.1:5000',
+        },
+      ],
+    },
+  };
 };
