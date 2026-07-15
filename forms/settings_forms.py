ac766b3cb87736e475bf059666db7028c2f30047
madhesh0727 <madhesh0727@gmail.com>
2026-07-15 21:54:52 +0530

    Fix missing FileField imports in forms

--- forms/settings_forms.py
+++ forms/settings_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, PasswordField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, SelectField, FileField
 from wtforms.validators import Optional, Email, URL, Length
 
 class SettingsForm(FlaskForm):
